d6b6de22b2a76b507f89ef9c27f00f5b00c0522f
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2020-08-10 18:17:52 -0700

    redux-2-afternoon

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux'
+import store from './ducks/store'
 
 ReactDOM.render(
   <App />
